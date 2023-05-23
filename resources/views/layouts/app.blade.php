71b54d8b4b161cd09e0975c1ad735687bda9c59e
Binod <ybinod857@gmail.com>
2023-05-24 01:18:50 +0545

    Logging out is done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,44 +8,50 @@
     <title>Posty</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
+
 <body>
-<body class="bg-gray-200 ">
-    <nav class="p-6 bg-white flex justify-between mb-6">
-        <ul class="flex items-center">
-            <li>
-                <a href="#" class="p-3">Home</a>
-            </li>
-            <li>
-                <a href="#" class="p-3" >Dashboard</a>
-            </li>
-            <li>
-                <a href="#" class="p-3">Post</a>
-            </li>
-        </ul>
-        <ul class="flex items-center">
-            <!-- if the user is sign in  -->
-            @auth
-            <li>
-                <a href="#" class="p-3">Binod Ray</a>
-            </li>
-            <li>
-                <a href="#" class="p-3">logout</a>
-            </li>
-            @endauth
 
-            @guest
-            <!-- if the user is not sign in -->
-            <li>
-                <a href="{{route('login')}}" class="p-3">Login</a>
-            </li>
-            <li>
-                <a href="{{route('register')}}" class="p-3">Register</a>
-            </li>
-            @endguest
+    <body class="bg-gray-200 ">
+        <nav class="p-6 bg-white flex justify-between mb-6">
+            <ul class="flex items-center">
+                <li>
+                    <a href="/" class="p-3">Home</a>
+                </li>
+                <li>
+                    <a href="{{route('dashboard')}}" class="p-3">Dashboard</a>
+                </li>
+                <li>
+                    <a href="#" class="p-3">Post</a>
+                </li>
+            </ul>
+            <ul class="flex items-center">
+                <!-- if the user is sign in  -->
+                @auth
+                <li>
+                    <a href="#" class="p-3">Binod Ray</a>
+                </li>
+                <li>
+                    <form action="{{route('logout')}}" method="post" class="p-3 inline">
+                        @csrf
+                        <button type="submit">Logout</button>
+                    </form>
+                </li>
+                @endauth
+
+                @guest
+                <!-- if the user is not sign in -->
+                <li>
+                    <a href="{{route('login')}}" class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href="{{route('register')}}" class="p-3">Register</a>
+                </li>
+                @endguest
 
 
-        </ul>
-    </nav>
-@yield('content')
-</body>
+            </ul>
+        </nav>
+        @yield('content')
+    </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,12 +16,18 @@ use App\Http\Controllers\Auth\LoginController;
 |
 */
 
-Route::get('/', function () {
+Route::get('/post', function () {
     return view('posts.index');
 });
 
+Route::get('/', function(){
+    return view('home');
+})->name('home');
+
 Route::get('/register',[RegisterController::class,'index'])->name('register');
 Route::post('/register',[RegisterController::class,'store']);
+
+Route::post('/logout',[LogoutController::class,'store'])->name('logout');
 
 Route::get('/login',[LoginController::class,'index'])->name('login');
 Route::post('/login',[LoginController::class,'store']);
