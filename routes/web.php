0490b4122022652dbbfb156c872f6e9439438f88
Binod <ybinod857@gmail.com>
2023-05-23 23:36:54 +0545

    Registering users done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts.index');
 });
+
+Route::get('/register',[RegisterController::class,'index'])->name('register');
+Route::post('/register',[RegisterController::class,'store']);
+
+Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
