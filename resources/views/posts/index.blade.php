c3f27a35362272eb7da11e0e2358b3523f606541
Binod <ybinod857@gmail.com>
2023-05-25 01:43:25 +0545

    Deleting posts and authorization is done

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -30,6 +30,16 @@
 
             <p class="mb-2">{{$post->body}}</p>
 
+            @can('delete',$post)
+            <div>
+                <form action="{{route('posts.destroy', $post)}}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="text-blue-500">Delete</button>
+                </form>
+            </div>
+            @endcan
+
             <div class="flex items-center">
                 @auth
                 @if(!$post->likedBy(auth()->user()))
