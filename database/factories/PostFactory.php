2389edd44f3d466e94c8b660974096a9b2b9da05
Binod <ybinod857@gmail.com>
2023-05-24 22:34:58 +0545

    Seeding with model factories done

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,19 +2,24 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class PostFactory extends Factory
 {
+
     /**
      * Define the model's default state.
      *
      * @return array
+     *
      */
+
+     protected $model = Post::class;
     public function definition()
     {
         return [
-            //
+            'body' => $this->faker->sentence(20)
         ];
     }
 }
